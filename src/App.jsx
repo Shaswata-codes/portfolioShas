c701073444f1c54d4801a4294989bfd6cb3e5861
Shaswata Sarkar <shaswatasarkar05@gmail.com>
2026-01-13 20:45:20 +0530

    Bg Animation added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import { useState } from 'react'
+import { useEffect } from "react"
+import { initTheme } from "./utils/theme"
+import AnimatedBackground from "./components/AnimatedBackground";
 
 function App() {
 
+  useEffect(() => {
+    initTheme();
+  },[])
+
   return (
-    <>
-      
-    </>
+    <div className="min-h-screen grid-pattern">
+      <AnimatedBackground/>
+    </div>
   )
 }
 
